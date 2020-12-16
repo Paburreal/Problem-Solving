9f64eea5fdc154a81f5baa3605174771eb1b9757
Paburreal <paburreal@gmail.com>
2020-12-15 18:04:26 -0800

    800
    https://codeforces.com/contest/158/problem/A

--- Codeforces/Next Round.py
+++ Codeforces/Next Round.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 """A next round"""
-n = int(input())
-k = int(input())
+"""Faster/Better input method"""
 
 def pass_next_round(n, k):
-    grades = []
     for i in range (0, n):
-        grades.append(int(input()))
         if grades[i] == 0:
             return i
         if i > k - 1 and grades[i] != grades[k - 1]:
             return i
     return n
+
+n, k = map(int,input().split())
+grades = list(map(int, input().split()))
 
 print(pass_next_round(n, k))
